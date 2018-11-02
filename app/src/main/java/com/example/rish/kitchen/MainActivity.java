cfb7c373e370fb5ff42a35261b6c0a5f4ea9b56a
Rishabh7838 <https://github.com/Rishabh7838/>
2018-11-03 01:21:29 +0530

    second

--- app/src/main/java/com/example/rish/kitchen/MainActivity.java
+++ app/src/main/java/com/example/rish/kitchen/MainActivity.java
@@ -118,18 +118,8 @@ private ProgressBar progressBar;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_shop) {
+            webView.loadUrl("https://www.fkitchens.com/shop/?v=c86ee0d9d7ed");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
